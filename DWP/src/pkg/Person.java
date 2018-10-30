b6bee267e69769cbad0d1368dd199e9084cef3cd
Ruth.Lennon <Ruth.Lennon@PR2244-RLLT.lyitnet.lyit.ie>
2018-10-30 21:53:36 +0000

    This is my first commit on this project!

---
+++ DWP/src/pkg/Person.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package pkg;
+
+/**
+ * @author Ruth.Lennon
+ *
+ */
+public class Person {
+	private int age;
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
